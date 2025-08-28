1306b171be1bbbb55e7b8896f564dcb6f6a16d5a
Bzkrtayse <aysebzkrt47@gmail.com>
2025-08-28 14:31:20 +0300

    added styles path to index.ts

--- index.ts
+++ index.ts
@@ -5,6 +5,8 @@ const PORT = 3000;
 const HOSTNAME = "localhost";
 
 const directoryPath = path.join(__dirname, 'src', 'pages');
+const stylesPath = path.join(__dirname, "src", "styles");
+
 
 async function readHTMLfile(res: ServerResponse, fileName: string) {
   const filePath = path.join(directoryPath, fileName);
@@ -18,8 +20,26 @@ async function readHTMLfile(res: ServerResponse, fileName: string) {
     res.end("500 Internal Server Error");
   }
 }
+async function serveCSS(res: ServerResponse, fileUrl: string) {
+  const filePath = path.join(stylesPath, path.basename(fileUrl));
+  try {
+    const data = await fs.readFile(filePath);
+    res.writeHead(200, { "Content-Type": "text/css" });
+    res.end(data);
+  } catch {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("404 CSS File Not Found");
+  }
+}
 
 const server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
+  
+  const url = req.url || "/";
+
+  if (url.startsWith("/styles/")) {
+    serveCSS(res, url);
+    return;
+  }
  
   if (req.url === "/" || req.url === "/home") {
     readHTMLfile(res, 'index.html');
